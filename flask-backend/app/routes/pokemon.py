36178d4f3971592f5dbb32cf4c9f16468e52a060
brandon <brandon.bim13@gmail.com>
2023-01-12 14:08:29 -0500

    completed model and made migrations

--- flask-backend/app/routes/pokemon.py
+++ flask-backend/app/routes/pokemon.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template, redirect
 from ..forms import PokemonForm
-from ..models import db, Pokemon, PokemonType, Items
+from ..models import db, Pokemon, PokemonType, Item
 
 pokemon_routes = Blueprint('pokemon', __name__)
 
 @pokemon_routes.route('/')
 def pokemon():
-    pass
+    pokemon = Pokemon.query.all()
+    print(pokemon)
+
+    return 'nothing'
